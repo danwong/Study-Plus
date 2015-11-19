672a1ca2658369a469b64d432b63f3bfc8d16e61
Kwuerch <kyle98678@gmail.com>
2015-11-19 14:52:43 -0800

    Added FinishedGameViewController
    Began work on making the group study function more fluid.

---
+++ FinishedGameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FinishedGameViewController.swift
+//  StudyPlus
+//
+//  Created by Student 2 on 11/19/15.
+//  Copyright © 2015 Charvi Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class FinishedGameViewController: UIViewController {
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
